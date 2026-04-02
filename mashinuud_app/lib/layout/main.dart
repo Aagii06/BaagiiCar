b6616579ee9afc1eba875bd100a58eea6b85b10b
Aagii06 <61139320+Aagii06@users.noreply.github.com>
2026-04-02 23:06:19 +0800

    sssss

---
+++ mashinuud_app/lib/layout/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../screens/home.dart'; // Нүүр хуудсаа дуудаж байна
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const Center(child: Text('Search')),
+    const Center(child: Text('Alerts')),
+    const Center(child: Text('Messages')),
+    const Center(child: Text('Profile')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Alerts',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
