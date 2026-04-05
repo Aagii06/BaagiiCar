8359c5edc7c94f3bfd110cc995a99b25defad2c2
Aagii06 <61139320+Aagii06@users.noreply.github.com>
2026-04-06 00:40:10 +0800

    ssss

---
+++ mashinuud_app/lib/data/car_brands.dart
@@ -0,0 +1,140 @@
+class CarBrand {
+  final String brand;
+  final String country;
+  final String logo;
+
+  const CarBrand({
+    required this.brand,
+    required this.country,
+    required this.logo,
+  });
+}
+
+const List<CarBrand> carBrands = [
+  CarBrand(
+    brand: "Toyota",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/toyota/toyota-icon.svg",
+  ),
+  CarBrand(
+    brand: "Honda",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/honda/honda-icon.svg",
+  ),
+  CarBrand(
+    brand: "Nissan",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/nissan/nissan-icon.svg",
+  ),
+  CarBrand(
+    brand: "Lexus",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/lexus/lexus-icon.svg",
+  ),
+  CarBrand(
+    brand: "Mazda",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/mazda/mazda-icon.svg",
+  ),
+  CarBrand(
+    brand: "Subaru",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/subaru/subaru-icon.svg",
+  ),
+  CarBrand(
+    brand: "Suzuki",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/suzuki/suzuki-icon.svg",
+  ),
+  CarBrand(
+    brand: "Mitsubishi",
+    country: "Japan",
+    logo: "https://www.vectorlogo.zone/logos/mitsubishi/mitsubishi-icon.svg",
+  ),
+  CarBrand(
+    brand: "Mercedes-Benz",
+    country: "Germany",
+    logo:
+        "https://www.vectorlogo.zone/logos/mercedesbenz/mercedesbenz-icon.svg",
+  ),
+  CarBrand(
+    brand: "BMW",
+    country: "Germany",
+    logo: "https://www.vectorlogo.zone/logos/bmw/bmw-icon.svg",
+  ),
+  CarBrand(
+    brand: "Audi",
+    country: "Germany",
+    logo: "https://www.vectorlogo.zone/logos/audi/audi-icon.svg",
+  ),
+  CarBrand(
+    brand: "Volkswagen",
+    country: "Germany",
+    logo: "https://www.vectorlogo.zone/logos/volkswagen/volkswagen-icon.svg",
+  ),
+  CarBrand(
+    brand: "Porsche",
+    country: "Germany",
+    logo: "https://www.vectorlogo.zone/logos/porsche/porsche-icon.svg",
+  ),
+  CarBrand(
+    brand: "Hyundai",
+    country: "South Korea",
+    logo: "https://www.vectorlogo.zone/logos/hyundai/hyundai-icon.svg",
+  ),
+  CarBrand(
+    brand: "Kia",
+    country: "South Korea",
+    logo: "https://www.vectorlogo.zone/logos/kia/kia-icon.svg",
+  ),
+  CarBrand(
+    brand: "Tesla",
+    country: "USA",
+    logo: "https://www.vectorlogo.zone/logos/tesla/tesla-icon.svg",
+  ),
+  CarBrand(
+    brand: "Ford",
+    country: "USA",
+    logo: "https://www.vectorlogo.zone/logos/ford/ford-icon.svg",
+  ),
+  CarBrand(
+    brand: "Chevrolet",
+    country: "USA",
+    logo: "https://www.vectorlogo.zone/logos/chevrolet/chevrolet-icon.svg",
+  ),
+  CarBrand(
+    brand: "Jeep",
+    country: "USA",
+    logo: "https://www.vectorlogo.zone/logos/jeep/jeep-icon.svg",
+  ),
+  CarBrand(
+    brand: "Land Rover",
+    country: "United Kingdom",
+    logo: "https://www.vectorlogo.zone/logos/landrover/landrover-icon.svg",
+  ),
+  CarBrand(
+    brand: "Bentley",
+    country: "United Kingdom",
+    logo: "https://www.vectorlogo.zone/logos/bentley/bentley-icon.svg",
+  ),
+  CarBrand(
+    brand: "Ferrari",
+    country: "Italy",
+    logo: "https://www.vectorlogo.zone/logos/ferrari/ferrari-icon.svg",
+  ),
+  CarBrand(
+    brand: "Lamborghini",
+    country: "Italy",
+    logo: "https://www.vectorlogo.zone/logos/lamborghini/lamborghini-icon.svg",
+  ),
+  CarBrand(
+    brand: "Volvo",
+    country: "Sweden",
+    logo: "https://www.vectorlogo.zone/logos/volvocars/volvocars-icon.svg",
+  ),
+  CarBrand(
+    brand: "BYD",
+    country: "China",
+    logo: "https://www.vectorlogo.zone/logos/byd/byd-icon.svg",
+  ),
+];
--- mashinuud_app/lib/screens/home.dart
+++ mashinuud_app/lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mashinuud_app/l10n/locale.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:mashinuud_app/l10n/locale.dart';
+
+import 'package:mashinuud_app/data/car_brands.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,10 +10,48 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          context.l10n.home_page,
-          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 40.0, horizontal: 16.0),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 60,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: carBrands.length,
+                  itemBuilder: (context, index) {
+                    final brand = carBrands[index];
+                    return Center(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(43)),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              blurRadius: 4,
+                              offset: const Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: SvgPicture.network(
+                          brand.logo,
+                          width: 40,
+                          height: 40,
+                          placeholderBuilder: (context) =>
+                              const CircularProgressIndicator(),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
